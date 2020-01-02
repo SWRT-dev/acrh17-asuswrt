7c3f0e45acfec9c7492996a6110dd2b3de6f58e1
ZhaoWei Xu <paldier@hotmail.com>
2020-01-02 13:34:40 +0800

    softcenter:fix missing "fi"

--- release/src/router/softcenter/jffsinit.sh
+++ release/src/router/softcenter/jffsinit.sh
@@ -24,6 +24,7 @@ if [ "$(nvram get sc_mount)" == 1 ];then
 	usb_disk="/tmp/mnt/$mdisk"
 	if [ "$(nvram get productid)" == "BLUECAVE" ];then
 		[ -n "$(mount |grep sda1 |grep tfat)" ] && logger "Unsupport TFAT!" && exit 1
+	fi
 	if [ ! -e "$usb_disk" ]; then
 		nvram set sc_mount="0"
 		nvram commit
